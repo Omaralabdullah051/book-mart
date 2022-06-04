7e17e2cccc5317b16ab31ba795b42f6fb113d85f
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-04 14:08:52 +0400

    update overall

--- README.md
+++ README.md
@@ -6,27 +6,26 @@ Livesite link: [bookMart](https://book-mart-94d39.web.app/).
 
 In the overall project, you will found:
 
- - I implemented authentication system to identify users (Email-password auth & Google auth).
+- I implemented authentication system to identify users (Email-password auth & Google auth).
 
- - I protected the Checkout page with help of authentication process.
+- I protected the Checkout page with help of authentication process.
 
- - I added many features and services for stocking inventory items.
+- I added many features and services for stocking inventory items.
 
- - I implemented JWT to ensure the authenticity and verify users. 
+- I implemented JWT to ensure the authenticity and verify users.
 
- - I added a Blogs page and write some significant questions answers.
+- I added a Blogs page and write some significant questions answers.
 
 ## List of teachnologies & functionality that I used and implemented in my project:
 
 In the overall project, you will found:
 
- - I used React Router DOM to set up Routes in my overall project.
+- I used React Router DOM to set up Routes in my overall project.
 
- - I implemented firebase & React Firebase Hooks to simplify my application's authentication system.
+- I implemented firebase & React Firebase Hooks to simplify my application's authentication system.
 
- - I used tailwind css with some customization in index.css and config file with some directives & flowbite.
+- I used tailwind css with some customization in index.css and config file with some directives & flowbite.
 
- - I installed React Fontawesome & hero icons to add some icons & react-helmet-async to set dynamic title.
+- I installed React Fontawesome & hero icons to add some icons & react-helmet-async to set dynamic title.
 
- - I installed React toastify to add some toast & also sweetalert2 to use confirmation popup. 
-
+- I installed React toastify to add some toast & also sweetalert2 to use confirmation popup.
--- src/Pages/ViewProfile/ViewProfile.js
+++ src/Pages/ViewProfile/ViewProfile.js
@@ -1,49 +1,68 @@
-import { signOut } from 'firebase/auth';
-import React, { useEffect } from 'react';
-import { useAuthState, useUpdateProfile } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
-import auth from '../../firebase.init';
-import PageTitle from '../Shared/PageTitle/PageTitle';
+import { signOut } from "firebase/auth";
+import React, { useEffect } from "react";
+import { useAuthState, useUpdateProfile } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
+import PageTitle from "../Shared/PageTitle/PageTitle";
 
 const ViewProfile = () => {
-    const [user] = useAuthState(auth);
-    const [updateProfile] = useUpdateProfile(auth);
-    const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+  const [updateProfile] = useUpdateProfile(auth);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!user) {
-            navigate('/');
-        }
-    }, [user, navigate]);
-
-    useEffect(() => {
-        if (!user?.photoURL) {
-            (async () => {
-                await updateProfile({ photoURL: "https://i.postimg.cc/mk6QDsQm/5945893.png" });
-            })();
-        }
-    }, [user, updateProfile]);
-
-
-    const handleNavigate = () => {
-        navigate('/info');
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
     }
+  }, [user, navigate]);
 
-    return (
-        <div className='bg-gray-800 m-5 md:m-10 rounded-2xl p-4 md:p-10'>
-            <PageTitle title="View Profile" />
-            <div>
-                <img className='w-40 mx-auto md:mx-0 rounded-full md:ml-20 xl:ml-32' src={user?.photoURL} alt="" />
-            </div>
-            <div className='text-green-600 mt-4 font-bold md:pl-20 xl:pl-32 md:mt-2 xl:pr-32'>
-                <h5><i>{user?.displayName}</i></h5>
-                <p><i>{user?.email}</i></p>
-                <p className='text-xs md:text-base text-gray-500 mt-4 text-justify'>{`Hey ${user?.displayName}! Hope you are doing great. Here you will find more other features. You can also find detailed and accurate information about the financial condition and performance in the last few years. It also allows you to know how much executives get paid and their lasts statements on conference calls. It also enables you to view financial statements that go back to five time periods.`}</p>
-                <button onClick={handleNavigate} className='px-3 py-1 md:px-8 md:py-2 text-xs md:text-base bg-green-600 text-gray-300 rounded font-bold mt-2  hover:bg-green-400 hover:text-black focus:ring-4 focus:ring-offset-slate-800'>Info</button>
-                <button onClick={() => signOut(auth)} className='px-3 py-1 md:px-8 md:py-2 text-xs md:text-base bg-green-600 text-gray-300 rounded font-bold mt-2  hover:bg-green-400 hover:text-black ml-4 md:ml-8 focus:ring-4 focus:ring-offset-slate-800'>Sign Out</button>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    if (!user?.photoURL) {
+      (async () => {
+        await updateProfile({
+          photoURL: "https://i.postimg.cc/mk6QDsQm/5945893.png",
+        });
+      })();
+    }
+  }, [user, updateProfile]);
+
+  const handleNavigate = () => {
+    navigate("/info");
+  };
+
+  return (
+    <div className="bg-gray-800 m-5 md:m-10 rounded-2xl p-4 md:p-10">
+      <PageTitle title="View Profile" />
+      <div>
+        <img
+          className="w-40 mx-auto md:mx-0 rounded-full md:ml-20 xl:ml-32"
+          src={user?.photoURL}
+          alt=""
+        />
+      </div>
+      <div className="text-green-600 mt-4 font-bold md:pl-20 xl:pl-32 md:mt-2 xl:pr-32">
+        <h5>
+          <i>{user?.displayName}</i>
+        </h5>
+        <p>
+          <i>{user?.email}</i>
+        </p>
+        <p className="text-xs md:text-base text-gray-500 mt-4 text-justify">{`Hey ${user?.displayName}! Hope you are doing great. Here you will find more other features. You can also find detailed and accurate information about the financial condition and performance in the last few years. It also allows you to know how much executives get paid and their lasts statements on conference calls. It also enables you to view financial statements that go back to five time periods.`}</p>
+        <button
+          onClick={handleNavigate}
+          className="px-3 py-1 md:px-8 md:py-2 text-xs md:text-base bg-green-600 text-gray-300 rounded font-bold mt-2  hover:bg-green-400 hover:text-black focus:ring-4 focus:ring-offset-slate-800"
+        >
+          Info
+        </button>
+        <button
+          onClick={() => signOut(auth)}
+          className="px-3 py-1 md:px-8 md:py-2 text-xs md:text-base bg-green-600 text-gray-300 rounded font-bold mt-2  hover:bg-green-400 hover:text-black ml-4 md:ml-8 focus:ring-4 focus:ring-offset-slate-800"
+        >
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ViewProfile;
